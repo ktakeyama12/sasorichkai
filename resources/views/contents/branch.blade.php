d1a0277c145941f85575fad8235dca1237404a7d
yuika.tomiyasu <yuika.tomiyasu@rakuten.com>
2018-07-10 05:14:42 +0000

    gallery update

--- resources/views/contents/branch.blade.php
+++ resources/views/contents/branch.blade.php
@@ -9,40 +9,46 @@
 
         <!-- Bootstrap CSS-->
         <link href="css/branch.css" rel="stylesheet" type="text/css">        <!-- jQuery -->
-        <script type="text/javascript" src="js/gallery.js"></script>
+       
     </head>
     <body>
         
         <h1>全国の楽天</h1>
-            
+        
+         <div>    
         <p>
             <img class="japan" src="/images/japan.jpg">
         </p>
-            
-            <ul>
-                <li>①札幌支社</li>
-                <li>②仙台支社</li>
-                <li>③盛岡オフィス</li>
-                <li>④新潟支社</li>
-                <li>⑤さいたま支社</li>
-                {!! link_to_route('contents.tokyo', '⑥楽天クリムゾンハウス') !!}
-                <li>⑦千葉支社</li>
-                <li>⑧横浜支社</li>
-                <li>⑨松本オフィス</li>
-                <li>⑩静岡支社</li>
-                <li>⑪名古屋支社</li>
-                <li>⑫金沢支社</li>
-                <li>⑬京都支社</li>
-                {!! link_to_route('contents.osaka', '⑭大阪支社') !!}
-                <li>⑮神戸支社</li>
-                <li>⑯広島支社</li>
-                <li>⑰松山支社</li>
-                {!! link_to_route('contents.fukuoka', '⑱福岡支社') !!}
-                <li>⑲鹿児島支社</li>
-                <li>⑳沖縄支社</li>
+        </div>
+       
+            <div class = "entry-content">
+            <ol>
+                <li>札幌支社</li>
+                <li>仙台支社</li>
+                <li>盛岡オフィス</li>
+                <li>新潟支社</li>
+                <li>さいたま支社</li>
+                <li>{!! link_to_route('contents.tokyo', '楽天クリムゾンハウス') !!}</li>
+                <li>千葉支社</li>
+                <li>横浜支社</li>
+                <li>松本オフィス</li>
+                <li>静岡支社</li>
+                <li>名古屋支社</li>
+                <li>金沢支社</li>
+                <li>京都支社</li>
+                <li>{!! link_to_route('contents.osaka', '大阪支社') !!}</li>
+                <li>神戸支社</li>
+                <li>広島支社</li>
+                <li>松山支社</li>
+                <li>{!! link_to_route('contents.fukuoka', '福岡支社') !!}</li>
+                <li>鹿児島支社</li>
+                <li>沖縄支社</li>
                 
         
-            </ul>
+            </ol>
+            </div>
+            
+            
         
     </body>
 </html>
