f4eb479b9e7c381376bb0c4525019234564bc9a4
kazuki.takeyama <kazuki.takeyama@rakuten.com>
2018-07-17 06:20:43 +0000

    Revert "bugfix"
    This reverts commit c8f8f80526a96deb190d6b625d140d26abf92fa9.

--- resources/views/workinfo.blade.php
+++ resources/views/workinfo.blade.php
@@ -9,15 +9,15 @@
         <script type="text/javascript" src="js/workinfo.js"></script>
         
         <!-- Bootstrap CSS-->
-        <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">-->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <!-- jQuery -->
-        <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>-->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
         <!-- Bootstrap JavaScript-->
-        <!--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>-->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     </head>
     
     <body>
-      <!--<button id="button" class="btn btn-danger" type="button" onclick="history.back()"  >戻る</button>-->
+      <button id="button" class="btn btn-danger" type="button" onclick="history.back()"  >戻る</button>
         <div class="cards">
 
   <div class="card">
