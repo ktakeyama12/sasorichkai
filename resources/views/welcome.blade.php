facb129938e01445ebdc846d852c813d26d2ebcb
Takuya.Yoda <ec2-user@ip-172-31-17-175.us-east-2.compute.internal>
2018-07-17 04:23:31 +0000

    game

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,7 +14,10 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
 
-     
+    <!--ロード画面 -->
+    
+    
+    
               <!--JQuery-->
               <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
     
@@ -29,13 +32,21 @@
 </head>
 
 <body>
+<div id="loader"><img src="images/panda.gif" alt="Loading..." /><br><center>Now Loading...</center></div>
+<div id="fade"></div>
+ 
+<div id="container" style="display:none">
+  
+  
   <header>
     <br>
     <div><img class="logo" src="/images/logo.jpg"></div>
     <h1>みんなに嬉しい、楽天プライム。</h1>
-  <h3 class="wakuwaku">― 毎日のお勤めにワクワクを ―</h3>
-  <br>
-  </header>
+  <h3 class="wakuwaku">― 楽天を使い倒せ！ ―</h3>
+  
+  <h4 >毎日のお勤めにワクワクをプラスする情報をお届けします。
+  <br>社員同士で交流できるサービスもご用意しています。</h4>
+</header>
   
   
 
@@ -44,31 +55,64 @@
   <!-- cont inner start -->
   <div class="cont__inner">
     <!-- el start -->
+    <!--<div class="el">-->
+    <!--  <div class="el__overflow">-->
+    <!--    <div class="el__inner">-->
+    <!--      <div class="el__bg"></div>-->
+    <!--      <div class="el__preview-cont">-->
+            
+    <!--        <h3 class="el__heading">ハッピー<br>手当<br></h3>-->
+    <!--      </div>-->
+    <!--      <div class="el__content">-->
+    <!--        <div class="el__text">-->
+    <!--          <div class="el__title"></div>-->
+
+              
+             
+                   
+    <!--<ol class="suuji">-->
+                   
+    <!--                  <li class="el__coupon"> <a href="rakuten"></a></li>-->
+    <!--                  <li class="coupon"> <a href="cafe" >Cafe & Restaurant around office</a></li>-->
+    <!--                  <li class="coupon"> <a href="movie">Movie & Karaoke</a></li>-->
+    <!--                  <li class="coupon"> <a href="amusement">Amusement park & pool</a></li>-->
+    <!--                  <li class="coupon"> <a href="shopping">Shopping</a></li>-->
+    <!--                 </ol>-->
+    <!--                 </div>-->
+                   
+                   
+                   
+                   
+                   
+                   
+
+    <!--        <div class="el__close-btn"></div>-->
+    <!--      </div>-->
+    <!--    </div>-->
+    <!--  </div>-->
+    <!--  <div class="el__index">-->
+    <!--    <div class="el__index-back"><img src="/images/rakuten_gallery/panda6.png"></div>-->
+    <!--    <div class="el__index-front">-->
+          <!--<div class="el__index-overlay" data-index="R"><img src="/images/rakuten_gallery/panda.png"></div>-->
+    <!--    </div>-->
+    <!--  </div>-->
+    <!--</div>-->
+    
+    <!-- el end -->
+    
     <div class="el">
       <div class="el__overflow">
         <div class="el__inner">
           <div class="el__bg"></div>
           <div class="el__preview-cont">
-            
-            <h3 class="el__heading">ハッピー<br>手当<br></h3>
+          <h3 class="el__heading">ハッピー<br>手当<br></h3>
           </div>
           <div class="el__content">
             <div class="el__text">
-              <!--<div class="el__title">Coupon Top5</div>-->
-
-              
-             
-            <p><h3>大きな出来事があると休みがもらえる可能性あり！<br>どんな時に休暇をとれるのかな？詳細をCheck！！</h3></p>
+            <h3>大きな出来事があると休みがもらえる可能性あり！<br>どんな時に休暇をとれるのかな？詳細をCheck！！</h3>
             
             {!! link_to_route('workinfo', '→ GO TO THE GALLERY') !!}
-                     </div>
-                   
-                   
-                   
-                   
-                   
-                   
-
+            </div>
             <div class="el__close-btn"></div>
           </div>
         </div>
@@ -81,7 +125,6 @@
       </div>
     </div>
     
-    <!-- el end -->
     <!-- el start -->
     <div class="el">
       <div class="el__overflow">
@@ -92,18 +135,18 @@
           </div>
           <div class="el__content">
             <div class="el__text">
-<div class="el__title">Coupon Top5</div>
+<div class="el__title"></div>
 
               
              
                    
     <ol class="suuji">
                    
-                      <li class="el__coupon"> <a href="rakuten">Rakuten services</a></li>
-                      <li class="coupon"> <a href="cafe" >Cafe & Restaurant around office</a></li>
-                      <li class="coupon"> <a href="movie">Movie & Karaoke</a></li>
-                      <li class="coupon"> <a href="amusement">Amusement park & pool</a></li>
-                      <li class="coupon"> <a href="shopping">Shopping</a></li>
+                      <li class="el__coupon"> <a href="rakuten">社内で使えるサービス</a></li>
+                      <li class="coupon"> <a href="cafe" >飲食店</a></li>
+                      <li class="coupon"> <a href="movie">映画 ＆ カラオケ</a></li>
+                      <li class="coupon"> <a href="amusement">アミューズ</a></li>
+                      <li class="coupon"> <a href="shopping">ショッピング</a></li>
                      </ol>
                      
               </div>
@@ -183,9 +226,7 @@
           </div>
           <div class="el__content">
             <div class="el__text">
-                          <p><h3>楽天社内の様子をご紹介。社員に人気のスポットや、隠れスポットを見てみよう！</h3></p>
-            
-            {!! link_to_route('contents.gallery', '→ GO TO THE GALLERY') !!}
+
 
             </div>
             <div class="el__close-btn"></div>
@@ -260,12 +301,12 @@
   <!-- cont inner end -->
 </div>
 
-  
+</div>  
   
 <!--ロードが完了してから読み込むJavascript-->
     <script  src="js/index.js"></script>
     <script src="js/chat.js"></script>
-
+    <script src="js/loader.js"></script>
 
 
 </body>
