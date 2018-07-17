30f864c0e921e281d756ad346bbcd633f5ef6599
kazuki.takeyama <kazuki.takeyama@rakuten.com>
2018-07-17 04:40:26 +0000

    favicon

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('password');
-            $table->integer('point');
+            $table->integer('point')->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
