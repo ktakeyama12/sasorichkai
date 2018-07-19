51ca9a33c8284e1322c7370c2318ecf3c0cfde00
kazuki.takeyama <kazuki.takeyama@rakuten.com>
2018-07-19 01:56:27 +0000

    point for page

--- app/Http/Controllers/AController.php
+++ app/Http/Controllers/AController.php
@@ -6,17 +6,31 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use App\User;
 
 class AController extends BaseController
 {
-        public function rakuten()
+    public function rakuten()
     {
+        $loginuser = \Auth::user();
+        $user = User::find($loginuser->id);
+        if($user->rakuten==0){
+            $user->rakuten = 1;
+            $user->save();
+        }
         return view('rakuten');
     }
     
     
      public function cafe()
     {
+        $loginuser = \Auth::user();
+        $user = User::find($loginuser->id);
+        if($user->cafe==0){
+            $user->cafe = 1;
+            $user->save();
+        }
+ 
         return view('cafe');
     }
     
@@ -29,21 +43,46 @@ class AController extends BaseController
 
      public function movie()
     {
+        $loginuser = \Auth::user();
+        $user = User::find($loginuser->id);
+        if($user->movie==0){
+            $user->movie = 1;
+            $user->save();
+        }
+
         return view('movie');
     }
     
     public function amusement()
     {
+        $loginuser = \Auth::user();
+        $user = User::find($loginuser->id);
+        if($user->amusement==0){
+            $user->amusement = 1;
+            $user->save();
+        }
         return view('amusement');
     }
     
      public function shopping()
     {
+        $loginuser = \Auth::user();
+        $user = User::find($loginuser->id);
+        if($user->shopping==0){
+            $user->shopping = 1;
+            $user->save();
+        }
         return view('shopping');
     }
     
     public function workinfo()
     {
+        $loginuser = \Auth::user();
+        $user = User::find($loginuser->id);
+        if($user->workinfo==0){
+            $user->workinfo = 1;
+            $user->save();
+        }
         return view('workinfo');
     }
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,16 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->integer('point')->default(0);
             $table->integer('prime')->default(0);
+            $table->integer('workinfo')->default(0);
+            $table->integer('rakuten')->default(0);
+            $table->integer('cafe')->default(0);
+            $table->integer('movie')->default(0);
+            $table->integer('amusement')->default(0);
+            $table->integer('shopping')->default(0);
+            $table->integer('branches')->default(0);
+            $table->integer('tokyo')->default(0);
+            $table->integer('osaka')->default(0);
+            $table->integer('fukuoka')->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
