39dc13eea413fbf11156afdf8cfed5f33dad8fc6
kazuki.takeyama <kazuki.takeyama@rakuten.com>
2018-07-20 01:09:06 +0000

    link

--- resources/views/cafe.blade.php
+++ resources/views/cafe.blade.php
@@ -33,7 +33,7 @@
             <li id="fullPreview"></li>
 
             <li>
-                <a href="images/gallery/p1.jpg"></a>
+                <a href="https://www.google.co.jp/search?num=30&hl=en-JP&rlz=1C1GGRV_enJP791JP791&biw=1396&bih=700&ei=8jRRW82RINPyhwPY5q3wCg&q=%E3%82%A2%E3%82%A4%E3%83%93%E3%82%A2%E3%83%BC%E3%83%AB%E3%80%80%E4%BA%8C%E5%AD%90%E7%8E%89%E5%B7%9D&oq=%E3%82%A2%E3%82%A4%E3%83%93%E3%82%A2%E3%83%BC%E3%83%AB%E3%80%80%E4%BA%8C%E5%AD%90%E7%8E%89%E5%B7%9D&gs_l=psy-ab.3..0i13k1j0i8i13i30k1.1970.4032.0.4449.14.14.0.0.0.0.167.1518.5j9.14.0....0...1c.1j4.64.psy-ab..5.5.585...0i13i30k1j0i10i30k1j33i160k1.0.bEK5-nFuuvE"></a>
                 <img data-original="images/gallery/drinking.jpg" src="img/effects/white.gif" alt="Photo 1" />
 
                 <div class="overLayer"></div>
@@ -62,7 +62,7 @@
                 <div class="projectInfo">McDonald's</div>
             </li>
             <li>
-                <a href="images/gallery/p3.jpg"></a>
+                <a href="https://www.google.co.jp/search?num=30&rlz=1C1GGRV_enJP791JP791&ei=HzVRW4XBH5f3hwPT5ZqYAQ&q=%E3%82%B7%E3%83%8A%E3%83%9C%E3%83%B3&oq=%E3%82%B7%E3%83%8A%E3%83%9C%E3%83%B3&gs_l=psy-ab.3..0i4k1l10.626.1948.0.2372.8.8.0.0.0.0.112.612.6j1.7.0....0...1c.1j4.64.psy-ab..1.7.611...0i67k1.0.x8SHcAtZdb8"></a>
                 <img data-original="images//gallery/cinamon.jpg" src="img/effects/white.gif" alt="Photo 3" />
 
                 <div class="overLayer"></div>
@@ -77,7 +77,7 @@
             </li>
             
             <li>
-                <a href="images/gallery/p3.jpg"></a>
+                <a href="https://www.google.co.jp/search?num=30&hl=en&ei=mjRRW7OTNMPbhwO9woeICw&q=%E3%82%AF%E3%82%A2%E3%82%A2%E3%82%A4%E3%83%8A&oq=%E3%82%AF%E3%82%A2%E3%82%A2%E3%82%A4%E3%83%8A&gs_l=psy-ab.3..35i39k1j0i4k1l9.10341.11870.0.12416.9.9.0.0.0.0.103.790.8j1.9.0....0...1c.1j4.64.psy-ab..0.9.787...0j0i131k1.0.8WVER3QktAY"></a>
                 <img data-original="images//gallery/hamburger.jpg" src="img/effects/white.gif" alt="Photo 3" />
 
                 <div class="overLayer"></div>
@@ -92,7 +92,7 @@
             </li>
             
             <li>
-                <a href="images/gallery/p3.jpg"></a>
+                <a href="https://www.google.co.jp/search?num=30&hl=en-JP&rlz=1C1GGRV_enJP791JP791&ei=HjVRW8KhIIKD8gXNvIuACA&q=bills+%E4%BA%8C%E5%AD%90%E7%8E%89%E5%B7%9D&oq=bills+%E4%BA%8C%E5%AD%90%E7%8E%89%E5%B7%9D&gs_l=psy-ab.3..0i4i37k1l5.28077.30735.0.31198.11.10.0.1.1.0.146.933.8j2.10.0....0...1c.1j4.64.psy-ab..1.10.861...0j35i39k1j0i203k1j0i4k1j0i10i30i42k1j0i8i4i30k1j0i8i4i10i30k1.0.ux826TE-jUk"></a>
                 <img data-original="images//gallery/bills.jpg" src="img/effects/white.gif" alt="Photo 3" />
 
                 <div class="overLayer"></div>
@@ -108,14 +108,14 @@
             
             
             <li>
-                <a href="images/gallery/p3.jpg"></a>
+                <a href="https://www.google.co.jp/search?q=%E3%81%99%E3%81%BF%E3%82%8C+%E4%BA%8C%E5%AD%90%E7%8E%89%E5%B7%9D&rlz=1C1GGRV_enJP791JP791&oq=%E3%81%99%E3%81%BF%E3%82%8C%E3%80%80%E3%81%AB&aqs=chrome.1.69i57j0l5.4853j0j9&sourceid=chrome&ie=UTF-8"></a>
                 <img data-original="images//gallery/sumire.jpg" src="img/effects/white.gif" alt="Photo 3" />
 
                 <div class="overLayer"></div>
                 <div class="infoLayer">
                     <ul>
                         <li><h3>すみれ</h3></li>
-                        <li><p>Riro</p></li>
+                        <li><p>Relo</p></li>
                         <li><p1>100円off</p1></li>
                     </ul>
                 </div>
@@ -123,7 +123,7 @@
             </li>
             
             <li>
-                <a href="images/gallery/p3.jpg"></a>
+                <a href="https://www.google.co.jp/search?num=30&rlz=1C1GGRV_enJP791JP791&ei=WDVRW7SCGY7N-QacnJLoCQ&q=%E6%B8%A9%E9%87%8E%E8%8F%9C&oq=%E6%B8%A9%E9%87%8E%E8%8F%9C&gs_l=psy-ab.3..0i67k1j0i4i37k1j0i67k1j0j0i4i37k1l3j0i67k1l2j0i4i37k1.13604.14499.0.14733.9.9.0.0.0.0.153.934.2j6.8.0....0...1c.1j4.64.psy-ab..3.6.709...35i39k1j0i4k1j0i131k1j0i4i10k1j0i4i10i42k1j0i10k1.0.cFfKsVOUcq8"></a>
                 <img data-original="images//gallery/pot.jpg" src="img/effects/white.gif" alt="Photo 3" />
 
                 <div class="overLayer"></div>
@@ -138,7 +138,7 @@
             </li>
             
             <li>
-                <a href="images/gallery/p3.jpg"></a>
+                <a href="https://www.google.co.jp/search?q=bubbys&rlz=1C1GGRV_enJP791JP791&oq=bubbys&aqs=chrome..69i57j0l5.2737j0j9&sourceid=chrome&ie=UTF-8"></a>
                 <img data-original="images//gallery/bubbys.jpg" src="img/effects/white.gif" alt="Photo 3" />
 
                 <div class="overLayer"></div>
