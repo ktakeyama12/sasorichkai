1830a47d63147e44c3944e8db8fc46963299f8f2
kazuki.takeyama <kazuki.takeyama@rakuten.com>
2018-07-24 05:06:49 +0000

    game

---
+++ resources/views/game.blade.php
@@ -0,0 +1,27 @@
+
+<!DOCTYPE html>
+<html lang="en" >
+
+
+<html lang="en">
+<head>
+  <meta name="author" content="www.twitter.com/cheeriottis">
+  <link href='https://fonts.googleapis.com/css?family=Pacifico' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="css/button.css">
+  <link rel="stylesheet" href="css/game.css">
+</head>
+<body>
+  <div class="towa_picture">
+  <img src="images/characters/hirayama.gif" class="hirayama">
+  <img src="images/characters/eigyouman.gif" class="eigyouman">
+  <img src="images/characters/kyaku.gif" class="kyaku">
+  </div>
+  
+  
+  <a href="hirayama" class="action-button shadow animate blue">営業のプロ、平山さんから学べ！</a>
+  <a href="#" class="action-button shadow animate red">ECCの基礎知識を学べ！</a>
+  <a href="soy" class="action-button shadow animate green">楽天市場で輝く店舗から学べ！</a>
+</body>
+</html>
+
+</html>
