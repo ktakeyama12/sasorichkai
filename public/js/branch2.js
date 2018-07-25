c614d0f36eeb6a28a5144be5fa414fe190181fd6
kazuki.takeyama <kazuki.takeyama@rakuten.com>
2018-07-25 04:14:32 +0000

    posfix

--- public/js/branch2.js
+++ public/js/branch2.js
@@ -3,7 +3,6 @@ var coordinates = function(element) {
     element2 = $('.japan');
     var childPos = element.offset();
     var parentPos = element2.offset();
-    console.log(parentPos)
     var childOffset = {
         top: childPos.top - parentPos.top,
         left: childPos.left - parentPos.left
@@ -12,13 +11,13 @@ var coordinates = function(element) {
     // var top = element.position().top - $(".japan").offset().top;
     // var left = element.position().left - $(".japan").offset().left;
     // $('#results').text('X: ' + left + ' ' + 'Y: ' + top);
-    if(childPos.left>170 && childPos.left<260 && childPos.top>150 && childPos.top<240){
+    if(childOffset.left>40 && childOffset.left<170 && childOffset.top>40 && childOffset.top<150){
         window.location.href = "tokyo";
     }
-    if(childPos.left>0 && childPos.left<100 && childPos.top>180 && childPos.top<290){
+    if(childOffset.left>-120 && childOffset.left<-30 && childOffset.top>80 && childOffset.top<190){
         window.location.href = "osaka";
     }
-    if(childPos.left>-200 && childPos.left<-110 && childPos.top>223 && childPos.top<325){
+    if(childOffset.left>-340 && childOffset.left<-200 && childOffset.top>130 && childOffset.top<240){
         window.location.href = "fukuoka";
     }
 }
