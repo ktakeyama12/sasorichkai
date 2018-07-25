8d1c14eef2c282b7c46fdf2c41c54d9e96a1339e
towa.sasaki <towa.sasaki@rakuten.com>
2018-07-25 04:43:34 +0000

    last commit again again

--- resources/views/quiz/ecc.blade.php
+++ resources/views/quiz/ecc.blade.php
@@ -14,6 +14,7 @@
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
       <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <script src="js/ecc.js"></script>
+    <titile>ECC Quiz</titile>
 </head>
 <body>
     <div id="bodyex">
