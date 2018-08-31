8091c0e7528c37858b6c18b99eaa99dbc8704b32
kazuki.takeyama <kazuki.takeyama@rakuten.com>
2018-08-31 07:04:49 +0000

    a

--- public/js/family.js
+++ public/js/family.js
@@ -255,7 +255,7 @@ function quizInterval(quizData, answerData1, answerData2, answerData3, answerDat
         answerSelected =  document.getElementById('leftPos').innerHTML;
         time--;
         console.log(i)
-        if(i==5){
+        if(i==6){
             clearInterval(timecounter);
             document.getElementById("game").style.display = "none";
             document.getElementById("finish").style.display = "block";
